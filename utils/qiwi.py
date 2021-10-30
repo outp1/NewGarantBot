292ede1c7db1b120c65f3941ddc3214280dae0d2
outp1 <outplayed2511@mail.ru>
2021-10-30 14:50:27 +0300

    'commit'

--- utils/qiwi.py
+++ utils/qiwi.py
@@ -1,16 +1,34 @@
 import asyncio
+#from data.config import QIWI_ST, QIWI_PHONE
 
 import aiohttp
+from SimpleQIWI import *
 
-async def create_bill():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://vk.com/feed') as response:
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+from datetime import datetime, timezone, timedelta
 
-            html = await response.text()
-            print('Body:', html, '...')
+token = '31093f320368ad6d9ada8b2ac9e2b7e9' #QIWI_ST
+phone = '+79189656523' #QIWI_PHONE
 
+
+class Qiwi():
+    def __init__(self, token, phone):
+        self.token = token
+        self.phone = phone
+
+    async def create_bill(self, amount, comment):
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Accept": "application/json"
+        }
+        async with aiohttp.ClientSession(headers=headers) as session:
+            data = {"amountInteger": amount, "currency": 643, "extra['comment']": comment, "extra['account']": self.phone, "extra['accountType']": 'nickname'}
+            async with session.get(f"https://qiwi.com/99999", params=data) as response:
+                print(response.real_url)
+
+
+
+
+qiwi = Qiwi(token, phone)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(create_bill())
+loop.run_until_complete(qiwi.create_bill('10', '213939'))
