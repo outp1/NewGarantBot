e38680669d90133b1808f51f8d07869afb667af4
outp1 <outplayed2511@mail.ru>
2021-11-09 20:49:36 +0300

    'commit'

--- handlers/groups/services.py
+++ handlers/groups/services.py
@@ -7,6 +7,9 @@ from filters import *
 @dp.message_handler(ServicesChat(), state='*')
 async def mentioning(message: types.Message):
     user = await bot.get_chat(message.from_user.id)
+    member = await bot.get_chat_member(message.chat.id, user.id)
+    if member.status in ['admin', 'creator', 'administrator']:
+        return
     if "@gnt_ebot" not in message.text:
         await bot.delete_message(message.chat.id, message.message_id)
         text=f'''
